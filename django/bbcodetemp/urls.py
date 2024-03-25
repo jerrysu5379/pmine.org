1ce499d0c898f3f203ac7a878ea170d5d36d42e9
とある科学のレッドストーン <73425737+Redstone-D@users.noreply.github.com>
2024-03-25 09:58:16 +0800

    BBCode Update

--- django/bbcodetemp/urls.py
+++ django/bbcodetemp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [ 
     path("", views.index, name="index"), 
-    #path("cinder", views.cinder, name="cinder"), 
-    #path("yan", views.yan, name="yan"), 
+    path("<str:temp>/", views.templates, name="templates"), 
+    path("<str:temp>/temp", views.viewtemp, name="viewtemp"), 
     #path("redstone", views.redstone, name="redstone"), 
 ] 
--- django/bbcodetemp/views.py
+++ django/bbcodetemp/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from op import language 
+from django.conf import settings 
 
 # Create your views here.
 
+BACK_URL = settings.ESSENTIALS + "bbcodetemp/" 
+
 def index(request): 
-    return(request, "https://bbcode.pmine.org/index.html") 
+    return render(request, "bbcodetemp/index.html", { 
+        'lang': language.get_language(request), 
+        'url': BACK_URL, 
+        'temp': "index", 
+    }) 
+
+def templates(request, temp): 
+    return render(request, f"bbcodetemp/{temp}/index.html", { 
+        'lang': language.get_language(request), 
+        'url': BACK_URL, 
+        'temp': temp, 
+    }) 
+
+def viewtemp(request, temp): 
+    return render(request, f"bbcodetemp/{temp}/temp.html", { 
+        'lang': language.get_language(request), 
+        'url': BACK_URL, 
+        'temp': temp, 
+    }) 
